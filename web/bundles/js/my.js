cd60accf1c96d6bf121b3b615613cc1d66c2a444
guyjstitt <guyjstitt@gmail.com>
2014-05-28 19:40:14 -0700

    fixed button bug

--- web/bundles/js/my.js
+++ web/bundles/js/my.js
@@ -4,10 +4,10 @@ $(document).ready(function() {
 
  $("title").text("Modified using Jquery");
  
- $('button').on('click', function() {
+ $('#num').on('click', function() {
 	var showNum = $('<span class="value"> 502-333-4283</span>');
-	$('.ul-left').append(showNum);
-	$('button').remove();
+	$('.ul-left span').last().after(showNum);
+	$('#num').remove();
 	});
  
  
